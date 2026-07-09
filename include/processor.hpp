d2598828b84695c51f65f5da1b90a8ad4aa289aa
Aashir27 <ahmed.aashir@outlook.com>
2026-07-10 03:19:00 +0500

    added alu, mux and instruction parser

--- include/processor.hpp
+++ include/processor.hpp
@@ -7,14 +7,12 @@
 #include <fstream>
 #include <cstdint>
 
-
-
-
 namespace RISC_V
 {
-    enum class ALU_OP : uint8_t {
-        ADD, 
-        SUB, 
+    enum class ALU_OP : uint8_t
+    {
+        ADD,
+        SUB,
         AND,
         OR,
         MUL,
@@ -24,14 +22,17 @@ namespace RISC_V
     };
     class Processor
     {
-        private:
-            std::array<int32_t, 32> registers;
-            uint32_t pc;
-            std::vector<std::string> memory;
-            std::vector<std::string> split (const std::string str, char delimiter);
-            std::string pick_chars (const std::string str, int start_idx, int end_idx);
-            
-        public:
-            Processor();
+    private:
+        std::array<int64_t, 32> registers;
+        uint64_t pc;
+        uint64_t alu(uint64_t op1, uint64_t op2, ALU_OP control_bits);
+        uint64_t mux(uint64_t a, uint64_t b, int select_bit);
+        std::vector<std::string> memory;
+        std::vector<uint32_t> instruction_parser(uint32_t instruction);
+        // std::vector<std::string> split (const std::string str, char delimiter);
+        // std::string pick_chars (const std::string str, int start_idx, int end_idx);
+
+    public:
+        Processor();
     };
 }
--- src/processor.cpp
+++ src/processor.cpp
@@ -2,51 +2,51 @@
 
 
 
-std::vector<std::string> RISC_V::Processor::split (const std::string str, char delimiter)
-{
-    std::vector<std::string> outstring;
-    std::string substring = "";
+// std::vector<std::string> RISC_V::Processor::split (const std::string str, char delimiter)
+// {
+//     std::vector<std::string> outstring;
+//     std::string substring = "";
 
-    if(delimiter == '\0')
-    {
-        for(char c : str)
-        {
-            outstring.push_back(std::string(1, c));
-        }
-    }
-    else
-    {
-        for(char c : str)
-        {
-        if(c == delimiter)
-        {
-            substring += c;
-        }
-        else
-        {
-            outstring.push_back(substring);
-            substring = "";
-        }
-        }
-        if(!substring.empty())  // To include ending characters if any
-        {
-            outstring.push_back(substring);
-        }
-    }
+//     if(delimiter == '\0')
+//     {
+//         for(char c : str)
+//         {
+//             outstring.push_back(std::string(1, c));
+//         }
+//     }
+//     else
+//     {
+//         for(char c : str)
+//         {
+//         if(c == delimiter)
+//         {
+//             substring += c;
+//         }
+//         else
+//         {
+//             outstring.push_back(substring);
+//             substring = "";
+//         }
+//         }
+//         if(!substring.empty())  // To include ending characters if any
+//         {
+//             outstring.push_back(substring);
+//         }
+//     }
     
-    return outstring;
-}
+//     return outstring;
+// }
 
-std::string RISC_V::Processor::pick_chars(const std::string str, int start_idx, int end_idx)
-{
-    if(start_idx < 0 && end_idx >= str.size())
-    {
-        throw std::invalid_argument("Index is out of bounds!");
-    }
+// std::string RISC_V::Processor::pick_chars(const std::string str, int start_idx, int end_idx)
+// {
+//     if(start_idx < 0 && end_idx >= str.size())
+//     {
+//         throw std::invalid_argument("Index is out of bounds!");
+//     }
     
-    int length = end_idx - start_idx + 1;
-    return str.substr(start_idx, length);
-}
+//     int length = end_idx - start_idx + 1;
+//     return str.substr(start_idx, length);
+// }
 
 RISC_V::Processor::Processor()
 {
@@ -68,6 +68,57 @@ RISC_V::Processor::Processor()
     }
     file.close();
 }
+
+
+uint64_t RISC_V::Processor::mux(uint64_t a, uint64_t b, int select_bit)
+{
+    if(select_bit == 0)
+    {
+        return a;
+    }
+    return b;
+}
+
+uint64_t RISC_V::Processor::alu(uint64_t op1, uint64_t op2, ALU_OP control_bits)
+{
+    switch (control_bits)
+    {
+        case ALU_OP::ADD: 
+            return op1 + op2;
+        case ALU_OP::AND: 
+            return op1 & op2;
+        case ALU_OP::MUL: 
+            return op1 * op2;
+        case ALU_OP::OR: 
+            return op1 | op2;
+        case ALU_OP::XOR: 
+            return op1 ^ op2;
+        case ALU_OP::SUB: 
+            return op1 - op2; 
+
+        default:
+            return 0;
+    }
+}
+
+std::vector<uint32_t> RISC_V::Processor::instruction_parser(uint32_t instruction)
+{
+    std::vector<uint32_t> fields;
+    uint32_t opcode = instruction & 127;   
+    uint32_t rd = (instruction >> 7) & 31;   // Shifting 7 bits to right, picking 5 bits as max value in 5 bits is 31 
+    uint32_t funct3 = (instruction >> 12) & 7; 
+    uint32_t rs1 = (instruction >> 15) & 31;
+    uint32_t rs2 = (instruction >> 20) & 31;
+    uint32_t funct7 = (instruction >> 25) & 127;
+    fields.push_back(opcode);
+    fields.push_back(rd);
+    fields.push_back(funct3);
+    fields.push_back(rs1);
+    fields.push_back(rs2);
+    fields.push_back(funct7);
+    return fields;
+}
+
 
 int main()
 {
